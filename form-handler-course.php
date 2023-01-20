649706c5b422cfd25f9f0259b4de884838d61f1a
CaioMenezes <caiofmen@hotmail.com>
2023-01-20 11:06:08 -0300

    Checking if all forms are running

---
+++ form-handler-course.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$mailTo = "caiofmen@gmail.com"
+$headers = "From: ".$visitor_email;
+$txt = "You have recived an e-mail from ".$name.".\n\n".$message;
+
+mail($mailTo, $txt, $headers);
+header("Location: contact.html?mailsend");
+}
+?>
