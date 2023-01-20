c439d88a823916c36dd36c7aad2d112923630fe0
CaioMenezes <caiofmen@hotmail.com>
2023-01-20 11:11:01 -0300

    Now it go

--- form-handler-course.php
+++ form-handler-course.php
@@ -2,11 +2,11 @@
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$comment = $_POST['comment'];
 
 $mailTo = "caiofmen@gmail.com"
 $headers = "From: ".$visitor_email;
-$txt = "You have recived an e-mail from ".$name.".\n\n".$message;
+$txt = "You have recived an e-mail from ".$name.".\n\n".$comment;
 
 mail($mailTo, $txt, $headers);
 header("Location: course.html?mailsend");
