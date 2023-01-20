c97993642312703d170664967259ecd796d8ece5
CaioMenezes <caiofmen@hotmail.com>
2023-01-20 10:42:21 -0300

    Trying to make php file work

--- form-handler.php
+++ form-handler.php
@@ -1,23 +1,15 @@
 <?php
+if (isset($_POST['submit'])) {
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'caiofmen@gmail.com'
-$email_subject = 'New Form Submission'
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
-              "User Message: $message.\n";
+$mailTo = "caiofmen@gmail.com"
+$headers = "From: ".$visitor_email;
+$txt = "You have recived an e-mail from ".$name.".\n\n".$message;
 
-$to = 'caiofmen@gmail.com'
-
-$headers = "From: $email_form \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact.html?mailsend");
+}
 ?>
